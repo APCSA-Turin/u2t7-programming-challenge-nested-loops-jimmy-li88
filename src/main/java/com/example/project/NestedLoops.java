169ea9c2923549ecd4e8108f25c46e072c18428e
Jimmy Li <jli8239@bths.edu>
2025-12-10 19:01:48 +0000

    save

--- src/main/java/com/example/project/NestedLoops.java
+++ src/main/java/com/example/project/NestedLoops.java
@@ -16,8 +16,17 @@ return the string below for given height=5
 ****
 *****  
            */
+          
 public static String starStaircase(int height){
-    return "";
+    String starStaircases = "";
+    for (int i = 0; i <= height; i++){
+        for (int k = 1; k <= i; k++){
+            starStaircases = starStaircases +  ("*") ;
+        }
+        starStaircases += "\n";
+    }
+    starStaircases = starStaircases.trim();
+    return starStaircases; 
 }
           /*
 return the string below for given height=5
@@ -29,7 +38,15 @@ return the string below for given height=5
            */
 
 public static String starStaircaseReverse(int height){
-    return "";
+    String reverseStairs= "";
+    for (int i = height; i >= 1; i--){
+        for (int k = 0; k < i; k++){
+            reverseStairs = reverseStairs + "*";
+        }
+        reverseStairs += "\n";
+    }
+    reverseStairs = reverseStairs.trim();
+    return reverseStairs;
 }
             /*
 
@@ -41,8 +58,21 @@ width=5 height=5
 *****
           */
     public static String emptyBox(int width, int height) {
-        return "";
+        String emptyBox = "";
+        for (int i = 0; i < width; i++){
+            emptyBox += "*";
+        }
+        for (int j = 0; j < height - 3; j++ ){
+            for (int k = 0; k < height - 3; k++){
+                emptyBox += ("*");
+            }
+        }
+        for (int l = 0; l < width; l++){
+            emptyBox += "*";
+        }
+        return emptyBox;
     }
+    
                     /*
 return the string below for word="HELLO" rows=5                    
 HELLO
--- src/main/java/com/example/project/NestedLoopsRunner.java
+++ src/main/java/com/example/project/NestedLoopsRunner.java
@@ -2,8 +2,19 @@ package com.example.project;
 
 public class NestedLoopsRunner {
     public static void main(String[] args) {
-     
+        String orig = " ";
+        String emptyBox = "";
+        for (int i = 0; i < 9; i++){
+            emptyBox += "*";
+        }
+        for (int j = 0; j < 5 - 3; j++ ){
+            for (int k = 0; k < 5 - 3; k++){
+                emptyBox += ("*" + "\n" + orig.repeat(7) + "*");
+            }
+        }
+        for (int l = 0; l < 9; l++){
+            emptyBox += "*";
+        }
+        System.out.print(emptyBox);
     }
 }
-
-  
